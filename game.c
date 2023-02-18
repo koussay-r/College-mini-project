376f83e41ec69281a1b4899ba6b0e5f8003e6e24
koussay-r <koussayr98@gmail.com>
2023-02-18 15:27:47 +0100

    update

--- game.c
+++ game.c
@@ -46,8 +46,8 @@ int chargerUnitesMagasinVersTableau(UniteMagasin unitesMagasin[MAX_LIGNES_UNITES
     }
     else{
         char line[100];
-        while(fgets(line,sizeof(line),file)!=NULL){
-            sscanf(line,"%d %d %d",&unitesMagasin[Numbr_lignes].idUniteMagasin,&unitesMagasin[Numbr_lignes].idFicheTypeUnite,&unitesMagasin[Numbr_lignes].idJoueurAutorise);
+        while(!feof(file)){
+            fscanf(file,"%d %d %d",&unitesMagasin[Numbr_lignes].idUniteMagasin,&unitesMagasin[Numbr_lignes].idFicheTypeUnite,&unitesMagasin[Numbr_lignes].idJoueurAutorise);
             Numbr_lignes++;
         }
         return Numbr_lignes;
@@ -64,8 +64,8 @@ int chargerVillagesVersTableau(Village villages[MAX_LIGNES_VILLAGES], char* nomF
     }
     else{
         char line[100];
-        while(fgets(line,sizeof(line),file)!=NULL){
-            sscanf(line,"%d %d %d %d",&villages[Numbr_lignes].idVillage,&villages[Numbr_lignes].ligne,&villages[Numbr_lignes].colonne,&villages[Numbr_lignes].idJoueurProprietaire);
+        while(!feof(file)){
+            fscanf(file,"%d %d %d %d",&villages[Numbr_lignes].idVillage,&villages[Numbr_lignes].ligne,&villages[Numbr_lignes].colonne,&villages[Numbr_lignes].idJoueurProprietaire);
             Numbr_lignes++;
         }
     }
@@ -81,8 +81,8 @@ int chargerTypesTerrainsVersTableau(TypeTerrain typesTerrains[NB_TYPES_TERRAINS]
     }
     else{
         char line[100];
-        while(fgets(line,sizeof(line),file)!=NULL){
-            sscanf(line,"%d %c %d %s",&typesTerrains[Numbr_lignes].idTypeTerrain,&typesTerrains[Numbr_lignes].symboleTerrain,&typesTerrains[Numbr_lignes].codeAffichageTerrain,&typesTerrains[Numbr_lignes].nomTerrain);
+        while(!feof(file)){
+            fscanf(file,"%d %c %d %s",&typesTerrains[Numbr_lignes].idTypeTerrain,&typesTerrains[Numbr_lignes].symboleTerrain,&typesTerrains[Numbr_lignes].codeAffichageTerrain,&typesTerrains[Numbr_lignes].nomTerrain);
             Numbr_lignes++;
         }
     }
@@ -98,9 +98,8 @@ int chargerPeriodesVersTableau(Periode periodes[MAX_LIGNES_PERIODES], char* nomF
     }
     else{
         char line[100];
-        while(fgets(line,sizeof(line),file)!=NULL){     
-            printf("%s",line);
-            sscanf(line,"%d %s %d %d %d",&periodes[Numbr_lignes].numOrdre,&periodes[Numbr_lignes].moment,&periodes[Numbr_lignes].bonus[0],&periodes[Numbr_lignes].bonus[1],&periodes[Numbr_lignes].bonus[2]);
+        while(!feof(file)){     
+            fscanf(file,"%d %s %d %d %d",&periodes[Numbr_lignes].numOrdre,&periodes[Numbr_lignes].moment,&periodes[Numbr_lignes].bonus[0],&periodes[Numbr_lignes].bonus[1],&periodes[Numbr_lignes].bonus[2]);
             Numbr_lignes++;
         }
     }
@@ -117,5 +116,6 @@ void main(){
     number_ligne_village=chargerVillagesVersTableau(villages,"./files/villages_original.txt");
     number_ligne_type_terrain=chargerTypesTerrainsVersTableau( typesTerrains,"./files/typesTerrains_original.txt");
     number_ligne_periode=chargerPeriodesVersTableau( periodes,"./files/periodes_original.txt");
+    printf("%d",number_lignes_unites_magasin);
     
 }
