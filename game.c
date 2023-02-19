f0c0a814cc3035ce490a50f83a66e2b6561f1a3d
koussay-r <koussayr98@gmail.com>
2023-02-19 15:34:31 +0100

    update

--- game.c
+++ game.c
@@ -14,6 +14,33 @@
 #define MAX_CAPACITES 3
 #define MAX_TRAITS 3
 typedef struct {
+int idFicheTypeUnite;
+int race; // humain, elfe, nain... voir la partie des races plus haut
+char nom[20]; // archer, cavalier...
+int prix;
+int pvMax; // points de vie de cette unité (HP)
+int mouvementsMax; // (MP)
+int xpRequise; // expérience requise pour passer au niveau suivant
+int niveau;
+int alignement; // 0 : loyal (lawful), 1 : chaotique (chaotic), 2 : liminal (liminal) ou 3 : neutre (neutral)
+int idFichesTypeUniteRangInf[NB_UNITES_RANG_INF_SUP]; // 0 si non existant
+int idFichesTypeUniteRangSup[NB_UNITES_RANG_INF_SUP]; // 0 si non existant
+Attaque attaques [MAX_ATTAQUES];
+int resistances[NB_RESISTANCES] ; // ordres des cases // blade, pierce, impact, fire, cold, arcane
+RelationTerrain relationsTerrains[NB_TYPES_TERRAINS]; // suivant les id : voir la partie terrains
+int idCapacites[MAX_CAPACITES]; // voir la partie des capacités plus haut, 0 sinon existante
+} FicheTypeUnite;
+typedef struct {
+int idAttaque; // Ces id sont des numéros d'ordre 1,2... Mettre 0 dans la case où on n’a pas d'attaque
+char nomAttaque[20];
+int degat;
+int nombreAttaques;
+int porteeAttaque; // 0 : melee , 1 : ranged
+int typeAttaque ; // 0 : blade, 1 : pierce, 2 : Impact, 3 : fire, 4 : cold, 5 : arcane
+int idAttaqueSpeciale1; // voir la partie des attaques spéciales plus haut
+int idAttaqueSpeciale2; // voir la partie des attaques spéciales plus haut
+} Attaque;
+typedef struct {
 int idUniteMagasin;// Ces id sont des numéros d'ordre 1,2... Mettre 0 dans la case où on n’a pas d'unité
 int idFicheTypeUnite;
 int idJoueurAutorise;
@@ -30,11 +57,52 @@ char symboleTerrain;
 int codeAffichageTerrain;
 char nomTerrain[20];
 } TypeTerrain;
+typedef struct {
+int idTypeTerrain;
+int coutMouvement;
+int defense;
+} RelationTerrain;
+typedef struct {
+int idJoueur;
+char symbole; // cas par défaut : J : joueur humain, E : joueur ennemi
+int or;
+int groupe_allies; // 1 : alliés joueur humain, 2 : alliés joueur pc ennemi
+int nombreVillages;
+int nombreUnites;
+int entretien;
+int revenu;
+} Joueur;
+typedef struct {
+int idUnite; // Ces id sont des numéros d'ordre 1,2... Mettre 0 dans la case où on n’a pas d'unité
+int idFicheTypeUnite;
+int idJoueur;
+char nomUnite[20];
+int active; // 1 si l’unité participe au jeu courant, 0 si l’unité ne participe pas on peut alors la rappeler
+int rang; // 0 : unité normale, 1 : unité chef
+int ligne;
+int colonne;
+int traits[MAX_TRAITS]; // voir la partie des traits plus haut
+int pv;
+int pvMax;
+int mouvements;
+int mouvementsMax;
+int xp;
+int xpRequise;
+int niveau;
+int finTour;
+} UniteJoueur;
+typedef struct {
+int idUnite;
+int idTypeTerrain;
+int codeAffichageTerrain; // code clavier du caractère à afficher dans la carte
+char symboleJoueur;
+int marque;
+} CelluleCarte;
 typedef struct
 {
 int numOrdre;
 char moment[20];
-int bonus[4]; // cases : 0 -> bonus lawful, 1 -> bonus chaotic, 2 -> bonus liminal, 3 -> bonus neutral
+int bonus[3]; // cases : 0 -> bonus lawful, 1 -> bonus chaotic, 2 -> bonus liminal, 3 -> bonus neutral
 } Periode;
 int chargerUnitesMagasinVersTableau(UniteMagasin unitesMagasin[MAX_LIGNES_UNITESMAGASIN], char* nomFichier){
     FILE *file;
@@ -125,9 +193,33 @@ void afficherTypesTerrains(int nb_lignes, TypeTerrain typesTerrains[NB_TYPES_TER
 }
 void afficherPeriodes(int nbLignes, Periode periodes[MAX_LIGNES_PERIODES]){
     for(int i=0;i<nbLignes;i++){
-        printf("%d %s %d \n",periodes[i].numOrdre,periodes[0].moment,periodes[i].bonus[0],periodes[i].bonus[1],periodes[i].bonus[2]);
+        printf("%d %s %d %d %d\n",periodes[i].numOrdre,periodes[0].moment,periodes[i].bonus[0],periodes[i].bonus[1],periodes[i].bonus[2]);
     }
     printf("\n");
+}
+void sauvegarderUnitesMagasin(int nb_lignes,UniteMagasin unitesMagasin[MAX_LIGNES_UNITESMAGASIN], char* nomFichier){
+    FILE *file=fopen(nomFichier,"w");
+    for(int i=0;i<nb_lignes;i++){
+        fprintf(file,"%d %d %d\n",unitesMagasin[i].idUniteMagasin,unitesMagasin[i].idFicheTypeUnite,unitesMagasin[i].idJoueurAutorise);
+    }
+}
+void sauvegarderVillages(int nb_lignes, Village villages[MAX_LIGNES_VILLAGES], char* nomFichier){
+    FILE *file=fopen(nomFichier,"w");
+    for(int i=0;i<nb_lignes;i++){
+        fprintf(file,"%d %d %d\n",villages[i].idVillage,villages[i].ligne,villages[i].colonne,villages[i].idJoueurProprietaire);
+    }
+}
+void sauvegarderTypesTerrains(int nb_lignes, TypeTerrain typesTerrains[NB_TYPES_TERRAINS],char* nomFichier){
+    FILE *file=fopen(nomFichier,"w");
+    for(int i=0;i<nb_lignes;i++){
+        fprintf(file,"%d %d %d %s\n",typesTerrains[i].idTypeTerrain,typesTerrains[i].symboleTerrain,typesTerrains[i].codeAffichageTerrain,typesTerrains[i].nomTerrain);
+    }
+}
+void sauvegarderPeriodes(int nbLignes, Periode periodes[MAX_LIGNES_PERIODES], char*nomFichier){
+    FILE *file=fopen(nomFichier,"w");
+    for(int i=0;i<nbLignes;i++){
+        fprintf(file,"%d %s %d %d %d \n",periodes[i].numOrdre,periodes[i].moment,periodes[i].bonus[0],periodes[i].bonus[1],periodes[i].bonus[2],periodes[i].bonus[3]);
+    }
 }
 void main(){
     char nomFichier;
@@ -144,5 +236,9 @@ void main(){
     afficherVillages(number_ligne_village, villages);
     afficherTypesTerrains(number_ligne_type_terrain,typesTerrains);
     afficherPeriodes(number_ligne_periode,periodes);
+    sauvegarderUnitesMagasin(number_lignes_unites_magasin,unitesMagasin,"unitesMagasin_sauvegarde.txt");
+    sauvegarderVillages(number_ligne_village,villages,"villages_sauvegarde.txt");
+    sauvegarderTypesTerrains(number_ligne_type_terrain,typesTerrains,"typesTerrains_sauvegarde.txt");
+    sauvegarderPeriodes(number_ligne_periode,periodes,"periodes_sauvegarde.txt");
 
 }
